395cbf307a23da42711c1181e430e2b3801b389b
Ismail Bugra <ibugraerdogan@gmail.com>
2025-05-16 18:55:14 +0300

    Küçük Büyük Harf Hassasiyeti Eklendi

--- assets/js/burc.js
+++ assets/js/burc.js
@@ -1,31 +1,33 @@
 let ay = prompt("Doğdunuz Ayı Giriniz 'ayın ilk hafini büyük yazınız örneğin Ocak,Şubat'");
 let gun = prompt("Doğduğunuz günü yazın (1-31 arası)");
 gun = Number(gun);
+
 ay = ay.toLowerCase();
-let burc = " ";
-if ((ay === "aralık" && gun >= 22) || (ay === "Ocak" && gun <= 21))
+
+let burc = "";
+if ((ay === "aralık" && gun >= 22) || (ay === "ocak" && gun <= 21))
 { burc = "Oğlak"; }
-else if ((ay === "Ocak" && gun >= 22 ) || (ay === "Şubat" && gun <= 19))
+else if ((ay === "ocak" && gun >= 22 ) || (ay === "şubat" && gun <= 19))
 { burc = "Kova"; }
-else if ((ay === "Şubat" && gun >= 20) || (ay === "Mart" && gun <= 20))
+else if ((ay === "şubat" && gun >= 20) || (ay === "mart" && gun <= 20))
 { burc = "Balık"; }
-else if ((ay === "Mart" && gun >= 21) || (ay === "Nisan" && gun <= 20))
+else if ((ay === "mart" && gun >= 21) || (ay === "nisan" && gun <= 20))
 { burc = "Koç"; }
-else if ((ay=== "Nisan" && gun >= 21 ) || (ay === "Mayıs" && gun <= 21))
+else if ((ay=== "nisan" && gun >= 21 ) || (ay === "mayıs" && gun <= 21))
 { burc = "Boğa"; }
-else if ((ay=== "Mayıs" && gun >= 22 ) || (ay === "Haziran" && gun <= 22))
+else if ((ay=== "mayıs" && gun >= 22 ) || (ay === "haziran" && gun <= 22))
 { burc = "İkizler"; }
-else if ((ay=== "Haziran" && gun >= 23 ) || (ay === "Temmuz" && gun <= 22))
+else if ((ay=== "haziran" && gun >= 23 ) || (ay === "temmuz" && gun <= 22))
 { burc = "Yengeç"; }
-else if ((ay=== "Temmuz" && gun >= 23 ) || (ay === "Ağustos" && gun <= 22))
+else if ((ay=== "temmuz" && gun >= 23 ) || (ay === "ağustos" && gun <= 22))
 { burc = "Aslan"; }
-else if ((ay=== "Ağustos" && gun >= 23 ) || (ay === "Eylül" && gun <= 22))
+else if ((ay=== "ağustos" && gun >= 23 ) || (ay === "eylül" && gun <= 22))
 { burc = "Başak :)) "; }
-else if ((ay=== "Eylül" && gun >= 23 ) || (ay === "Ekim" && gun <= 22))
+else if ((ay=== "eylül" && gun >= 23 ) || (ay === "ekim" && gun <= 22))
 { burc = "Terazi :)) "; }
-else if ((ay=== "Ekim" && gun >= 23 ) || (ay === "Kasım" && gun <= 21))
+else if ((ay=== "ekim" && gun >= 23 ) || (ay === "kasım" && gun <= 21))
 { burc = "Akrep"; }
-else if ((ay=== "Kasım" && gun >= 22 ) || (ay === "Akrep" && gun <= 21))
+else if ((ay=== "kasım" && gun >= 22 ) || (ay === "aralık" && gun <= 21))
 { burc = "Yay"; }
 else { burc = "Geçersiz giriş ayını doğru yazın."; }
 alert("merhaba " + burc + " burcusun"); 
